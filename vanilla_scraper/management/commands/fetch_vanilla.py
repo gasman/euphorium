81a03712aa49510a9987b2001629a59fbef23cc8
Matt Westcott <matt@west.co.tt>
2016-01-02 18:03:06 +0000

    Use BaseCommand instead of deprecated NoArgsCommand

--- vanilla_scraper/management/commands/fetch_vanilla.py
+++ vanilla_scraper/management/commands/fetch_vanilla.py
@@ -1,10 +1,10 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from forums.models import Site
 from vanilla_scraper.scraper import scrape
 
 
-class Command(NoArgsCommand):
-	def handle_noargs(self, **options):
-		for site in Site.objects.filter(engine='vanilla'):
-			scrape(site)
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for site in Site.objects.filter(engine='vanilla'):
+            scrape(site)
